47897af7101fde950ff2b81d174eb722591439ce
Johan Schuijt <johan.schuijt@ftm.nl>
2024-10-01 16:59:16 +0200

    trajectory change

--- src/lib/Scene.svelte
+++ src/lib/Scene.svelte
@@ -5,15 +5,15 @@
 <style>
 	div {
 		transform-style: preserve-3d;
-		animation: rotate 8s linear infinite;
+		animation: rotate 10s linear infinite;
 	}
 
 	@keyframes rotate {
 		from {
-			transform: rotateY(20deg) rotateX(-20deg) translateZ(0);
+			transform: rotateY(40deg) rotateX(-30deg) translateZ(-40rem);
 		}
 		to {
-			transform: rotateY(10deg) rotateX(-10deg) translateZ(100rem);
+			transform: rotateY(10deg) rotateX(-10deg) translateZ(80rem);
 		}
 	}
 </style>
